89fea67b48133d9a1dc32b4848f7e0c8aacf5676
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-11 17:53:04 +0530

    added some style to menubar

--- frontend/foodies/src/components/Menubar/Menubar.jsx
+++ frontend/foodies/src/components/Menubar/Menubar.jsx
@@ -7,7 +7,7 @@ const Menubar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container">
-   <img src={assets.logo} alt="" className='logo' height={48} width={48} />
+   <img src={assets.logo} alt="" className='mx-4' height={48} width={48} />
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -23,7 +23,7 @@ const Menubar = () => {
           <a className="nav-link" href="#">Contact us</a>
         </li>
       </ul>
-      <div className="menubar-right">
+      <div className="d-flex align-items-center gap-4">
         <div className="position-relative">
             <img src={assets.cart} alt="" height={32} width={32} className='position-relative'/>
             <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>7</span>
