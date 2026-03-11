e03480dbb043014d16fdda21f44ad580e9e4f1fb
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-11 17:48:05 +0530

    Added some basic things like home , login, register in menubar

--- frontend/foodies/src/components/Menubar/Menubar.jsx
+++ frontend/foodies/src/components/Menubar/Menubar.jsx
@@ -1,41 +1,37 @@
 import React from 'react';
 import './Menubar.css';
+import { assets } from '../../assets/assets';
+
 
 const Menubar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+  <div className="container">
+   <img src={assets.logo} alt="" className='logo' height={48} width={48} />
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link"  href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Explore</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <li className="nav-item">
+          <a className="nav-link" href="#">Contact us</a>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <div className="menubar-right">
+        <div className="position-relative">
+            <img src={assets.cart} alt="" height={32} width={32} className='position-relative'/>
+            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning'>7</span>
+        </div>
+        <button className='btn btn-outline-primary'>Login</button>
+        <button className='btn btn-outline-success'>Register</button>
+      </div>
+      
     </div>
   </div>
 </nav>
