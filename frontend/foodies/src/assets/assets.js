d7c35d377782e284ee531ffe12c98f25ce79bb0f
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-11 19:40:20 +0530

    created a Component and added top category foods

--- frontend/foodies/src/assets/assets.js
+++ frontend/foodies/src/assets/assets.js
@@ -1,7 +1,41 @@
 import logo from './logo.png';
 import cart from './cart.png';
 
+import burgur from './burgur.png';
+import cake from './cake.png';
+import icecream from './icecream.png';
+import pizza from './pizza.png';
+import rolls from './rolls.png';
+import salad from './salad.png';
+
+
 export const assets={
     logo,
     cart
 }
+
+export const categories = [
+    {
+        category:'Burgur',
+        icon:burgur
+    },
+     {
+        category:'Cake',
+        icon:cake
+    },
+     {
+        category:'Ice cream',
+        icon:icecream
+    },
+     {
+        category:'Pizza',
+        icon:pizza
+    },
+     {
+        category:'Rolls',
+        icon:rolls
+    }, {
+        category:'Salad',
+        icon:salad
+    }
+];
