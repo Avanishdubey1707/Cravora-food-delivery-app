29e5cb78dc97aad58db5d1d32b6536f70d05cd4a
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-12 19:55:54 +0530

    foodService created

--- frontend/foodies/src/context/StoreContext.jsx
+++ frontend/foodies/src/context/StoreContext.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) =>{
-    const contextValue={
 
+    const [foodList, setFoodList] = useState([]);
+   
+    const contextValue={
+        foodList
     };
+
+    useEffect(() => {
+        async function loadData() {
+           const data = await fetchFoodList();
+           setFoodList(data);
+            
+        }
+        loadData();
+    },[]);
+
+
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
