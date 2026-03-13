ed2a5f59938fd0e81437cdb638470b98a017bf4f
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-14 02:09:40 +0530

    login authentication implemebted

--- backend/foodiesapi/src/main/java/in/putin/foodiesapi/config/SecurityConfig.java
+++ backend/foodiesapi/src/main/java/in/putin/foodiesapi/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,9 +20,14 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import in.putin.foodiesapi.service.AppUserDetailsService;
+import lombok.AllArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private final AppUserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http 
@@ -55,6 +63,14 @@ public class SecurityConfig {
 
         return source;
       
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(){
+           DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+           authProvider.setUserDetailsService(userDetailsService);
+           authProvider.setPasswordEncoder(passwordEncoder());
+           return new ProviderManager(authProvider);
+
     }
 
 }
