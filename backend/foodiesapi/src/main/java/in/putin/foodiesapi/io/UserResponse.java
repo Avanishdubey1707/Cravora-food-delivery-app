bdf432a5403b14817e47c8e8568702de5e3e291c
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-13 23:16:51 +0530

    created userentity ,repository and many authentication java files

---
+++ backend/foodiesapi/src/main/java/in/putin/foodiesapi/io/UserResponse.java
@@ -0,0 +1,16 @@
+package in.putin.foodiesapi.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+
+    private String id;
+    private String name;
+    private String email;
+
+}
