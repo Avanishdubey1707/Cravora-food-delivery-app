f88793072cef122939b3afb7916fcbfe4ad3ef32
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-14 17:57:00 +0530

    Cartentity created

---
+++ backend/foodiesapi/src/main/java/in/putin/foodiesapi/entity/CartEntity.java
@@ -0,0 +1,30 @@
+package in.putin.foodiesapi.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "carts")
+public class CartEntity {
+    
+    private String id;
+
+    private String userId;
+
+    private Map<String , Integer> items = new HashMap<>();
+
+    public CartEntity(String userId,Map<String , Integer> items){
+        this.userId=userId;
+        this.items = items;
+    }
+
+}
