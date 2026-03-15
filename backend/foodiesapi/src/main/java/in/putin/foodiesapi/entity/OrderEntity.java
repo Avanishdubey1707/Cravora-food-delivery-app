b27acd968c67bb33b3b6c28d41b8d750a3538678
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-15 05:43:28 +0530

    Created OrderEntity

---
+++ backend/foodiesapi/src/main/java/in/putin/foodiesapi/entity/OrderEntity.java
@@ -0,0 +1,30 @@
+package in.putin.foodiesapi.entity;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import in.putin.foodiesapi.io.OrderItem;
+import lombok.Builder;
+import lombok.Data;
+
+@Document
+@Data
+@Builder
+public class OrderEntity {
+    @Id
+    private String id;
+    private String userId;
+    private String userAddress;
+    private String phoneNumber;
+    private String email;
+    private List<OrderItem> orderedItems;
+    private String amount;
+    private String paymentStatus;
+    private String razorpayOrderId;
+    private String razorpaySignature;
+    private String orderStatus;
+
+
+}
