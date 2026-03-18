8b27983dfbbf320398626e0efafcb8be4d933b42
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-19 02:03:19 +0530

    Foodies backend ready for deployment

--- backend/foodiesapi/src/main/java/in/putin/foodiesapi/FoodiesapiApplication.java
+++ backend/foodiesapi/src/main/java/in/putin/foodiesapi/FoodiesapiApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodiesapiApplication {
 
-	public FoodiesapiApplication() {
-		super();
-		//TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodiesapiApplication.class, args);
