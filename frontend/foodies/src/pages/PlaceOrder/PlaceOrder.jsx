c391ac094dc47ff0d4625bc1e7f2fa9e59feea76
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-29 22:50:38 +0530

    some new thing

--- frontend/foodies/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/foodies/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -161,7 +161,7 @@ const PlaceOrder = () => {
                 <div className="row g-5">
                     <div className="col-md-5 col-lg-4 order-md-last">
                         <h4 className="d-flex justify-content-between align-items-center mb-3">
-                            <span className="text-primary">Your cart</span>
+                            <span className="text-primary">Your Cart</span>
                             <span className="badge bg-primary rounded-pill">{cartItems.length}</span>
                         </h4>
                         <ul className="list-group mb-3">
